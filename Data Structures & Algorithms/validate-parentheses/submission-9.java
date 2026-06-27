ed1e5e1d8ce4443e50543fc0820068461a2d56c7
Haramrit Singh Khurana <haramrit09k@gmail.com>
2026-06-27 16:05:43 -0500

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        // with stack
+        Stack<Character> st = new Stack<>();
+
+        if (s.length() < 2 || s.length() == 0 || s == null)
+            return false; // less than 2 chars implies no matching pairs
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '[' || ch == '{' || ch == '(') {
+                // push on stack
+                st.push(ch);
+            } else {
+                if (st.isEmpty()) {
+                    return false;
+                }
+                char top = st.pop();
+                if (top == '[' && ch != ']')
+                    return false;
+                else if (top == '{' && ch != '}')
+                    return false;
+                else if (top == '(' && ch != ')')
+                    return false;
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
