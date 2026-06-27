82f505fedc2042d1ff4ba051c0c5072019be6af0
Haramrit Singh Khurana <haramrit09k@gmail.com>
2026-06-27 15:50:14 -0500

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isValid(String s) {
+        // brute force approach
+        while(s.contains("()") || s.contains("{}") || s.contains("[]")){
+            s = s.replace("()", "");
+            s = s.replace("{}", "");
+            s = s.replace("[]", "");
+        }
+        return s.isEmpty();
+    }
+}
